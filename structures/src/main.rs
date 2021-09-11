bf74da375ba3aa669428f13cf0117e1b4a16dfa3
yoshiaki-ki <wp.yk68td@gmail.com>
2021-09-11 14:57:05 +0900

    5. Structs

---
+++ structures/src/main.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    let rect3 = Rectangle {
+        width: 40,
+        height: 80,
+    };
+
+    println!("rect1 is {:#?}", rect1);
+    println!("The area of the rectangle is {}", rect1.area());
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+// 関数形式：area
+// fn area(rectungle: &Rectangle) -> u32 {
+//     rectungle.width * rectungle.height
+// }
